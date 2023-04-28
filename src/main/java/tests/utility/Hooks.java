7e9174fde1159941b85f38b90d781f9068f2c708
Vladislav Laptev <vlaptev@C02DTEBGML7H.restorationhardware.com>
2023-04-28 14:18:24 -0700

    Fixed tests

--- src/main/java/tests/utility/Hooks.java
+++ src/main/java/tests/utility/Hooks.java
@@ -62,11 +62,9 @@ public class Hooks {
      */
     private void ConfigFileReader() {
 
-//        profile = System.getenv("ENVIRONMENT");
-//        cookie = System.getenv("ENDPOINT");
+        profile = System.getenv("ENVIRONMENT");
+        cookie = System.getenv("ENDPOINT");
 
-        profile = "stg2";
-        cookie = "releasetues";
         if (profile == null) {
             Assert.fail("Environment Variable is NOT Set");
         } else {
