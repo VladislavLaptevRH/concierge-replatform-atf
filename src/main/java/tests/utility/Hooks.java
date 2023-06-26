d2e867a62c4c98976fc730cd02bd3ed4b1f8df77
mdovbenco <98607311+mdovbenco@users.noreply.github.com>
2023-06-26 20:45:23 +0300

    fix tests

--- src/main/java/tests/utility/Hooks.java
+++ src/main/java/tests/utility/Hooks.java
@@ -77,9 +77,9 @@ public class Hooks {
             System.out.println("Tests are running without cookie or endpoint");
         } else {
             System.out.println("Tests are running with endpoint = " + cookie);
+            System.out.println("Test are running for the following country" + country);
         }
 
-        System.out.println("Test are running for the following country" + country);
 
         BufferedReader reader;
         try {
