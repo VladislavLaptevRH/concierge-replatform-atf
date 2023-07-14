f4239ee24d1c352a84d0ca6984151f78e11c6c3e
mdovbenco <98607311+mdovbenco@users.noreply.github.com>
2023-07-14 16:23:28 +0300

    add parallel execution

--- src/main/java/tests/utility/Hooks.java
+++ src/main/java/tests/utility/Hooks.java
@@ -173,7 +173,7 @@ public class Hooks {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1366x768";
         Configuration.headless = true;
-        Configuration.pageLoadStrategy = "none";
+        Configuration.pageLoadStrategy = "normal";
         Configuration.timeout = 30000;
         Configuration.reportsFolder = "target/screenshots";
         open(url);
