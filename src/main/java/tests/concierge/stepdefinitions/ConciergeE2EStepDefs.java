0ea1ca4a974c3b9898442dd485118532ed22c3b6
Mukthar Umar Ali <mumarali@restorationhardware.com>
2023-10-09 09:14:41 -0700

    resolve comment

--- src/main/java/tests/concierge/stepdefinitions/ConciergeE2EStepDefs.java
+++ src/main/java/tests/concierge/stepdefinitions/ConciergeE2EStepDefs.java
@@ -1426,4 +1426,10 @@ public class ConciergeE2EStepDefs {
             System.out.println();
         }
     }
+
+    @Then("I verify multiSKU added in project")
+    public void iVerifyMultiSKUinProject(){
+        with().pollInterval(5, SECONDS).await().until(() -> true);
+        $(By.xpath("//*[text() = 'prod9300694']")).should(visible, Duration.ofSeconds(15));
+    }
 }
--- src/main/resources/features/concierge/ConciergeMultiSKU.feature
+++ src/main/resources/features/concierge/ConciergeMultiSKU.feature
@@ -83,6 +83,7 @@ Feature: Multi SKU
   Then I chose the '1' line item selections one by one
   When I click on add to project button
   When I click on go to project button
+  Then I verify multiSKU added in project
   Then I verify trade prices for "project page"
 
 
