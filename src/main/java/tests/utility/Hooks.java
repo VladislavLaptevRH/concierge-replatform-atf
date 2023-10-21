fc653d3b923d74cecb18f0d84fa221b8d0c32868
Mike Dovbenco <mikedovbenco@Mikes-MacBook-Pro.local>
2023-10-21 23:08:52 +0300

    fix issue with reporting

--- src/main/java/tests/utility/Hooks.java
+++ src/main/java/tests/utility/Hooks.java
@@ -60,13 +60,9 @@ public class Hooks {
      * This method get properties from application.properties file
      */
     private void ConfigFileReader() {
-//        profile = System.getenv("ENVIRONMENT");
-//        cookie = System.getenv("ENDPOINT");
-//        country = System.getenv("COUNTRY");
-
-        profile = "stg2";
-        country = "US";
-        cookie = "contentfix";
+        profile = System.getenv("ENVIRONMENT");
+        cookie = System.getenv("ENDPOINT");
+        country = System.getenv("COUNTRY");
 
         if (profile == null) {
             Assert.fail("Environment Variable is NOT Set");
